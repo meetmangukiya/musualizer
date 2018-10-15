6f0e630008e9e186e06779b62936b44a1d006932
Meet Mangukiya <meetmangukiya98@gmail.com>
2018-10-15 12:33:32 +0530

    Simplistic UI: Getting and reading the mp3 file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import 'audio-decode' as decode;
+import 'audio-lena/mp3' as buffer;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'musualizer';
+
+  inputOnChange() {
+    let file: File = document.querySelector("input[name='file']").files[0];
+    let fileReader: FileReader = new FileReader();
+    fileReader.onload = () {
+      let contents = fileReader.result;
+      this.performAnalysis(contents);
+    }
+
+    fileReader.readAsBinaryString(file);
+  }
+
+  performAnalysis(content) {
+    let p = document.createElement("p");
+    p.innerHTML = content;
+    document.getElementsByTagName("body")[0].appendChild(p);
+
+    console.log(content);
+  }
 }
